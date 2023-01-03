0fb6d698f9635fef20d21763c55a9e6f6e2e407b
Your Name <96727867+akhilswarop@users.noreply.github.com>
2023-01-03 15:21:15 +0530

    Let's start bro smh

---
+++ proj/proj/Site1/Page-1.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="ADDING A HOUSE TO THE DATABASE">
+    <meta name="description" content="">
+    <title>Page 1</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Page-1.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.1.5, nicepage.com">
+    <meta name="referrer" content="origin">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/TWAD-Board-Recruitment.png"
+}</script>
+    <meta name="theme-color" content="#0149b8">
+    <meta property="og:title" content="Page 1">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-header" id="sec-1607"><div class="u-clearfix u-sheet u-valign-middle-xl u-sheet-1">
+        <a href="ADMIN.html" class="u-active-black u-border-1 u-border-active-black u-border-grey-30 u-border-hover-black u-btn u-btn-round u-button-style u-gradient u-hover-black u-none u-radius-5 u-text-active-white u-text-hover-white u-btn-1">ADMIN</a>
+        <a href="Home.html" class="u-image u-logo u-image-1" data-image-width="500" data-image-height="500" title="Home">
+          <img class="u-logo-image u-logo-image-1" src="images/TWAD-Board-Recruitment.png">
+        </a>
+        <a href="VENDOR.html" class="u-active-black u-border-1 u-border-active-black u-border-grey-30 u-border-hover-black u-btn u-btn-round u-button-style u-gradient u-hover-black u-none u-radius-5 u-text-active-white u-text-hover-white u-btn-2">VENDOR</a>
+        <a href="Home.html#sec-77dc" class="u-active-black u-border-1 u-border-active-black u-border-grey-30 u-border-hover-black u-btn u-btn-round u-button-style u-gradient u-hover-black u-none u-radius-5 u-text-active-white u-text-hover-white u-btn-3">CONSUMER</a>
+      </div></header>
+    <section class="u-align-center u-clearfix u-gradient u-section-1" id="sec-aca8">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-shape u-shape-svg u-text-black u-shape-1">
+          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-e899"></use></svg>
+          <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-e899" style="enable-background:new 0 0 160 160;"><path d="M0,153.3L153.3,0h6.7L0,160V153.3z M129.6,0h6.7L0,136.3v-6.7L129.6,0z M105.6,0h6.7L0,112.3v-6.7L105.6,0z M81.6,0h6.7
+	L0,88.3v-6.7L81.6,0z M57.6,0h6.7L0,64.3v-6.7L57.6,0z M33.3,0H40L0,40v-6.7L33.3,0z M9.6,0h6.7L0,16.3V9.6L9.6,0z M18.9,160
+	L160,18.9v6.7L25.6,160H18.9z M44.5,160L160,44.5v6.7L51.2,160H44.5z M70.2,160L160,70.2V77l-83,83H70.2z M95.8,160L160,95.8v6.7
+	L102.6,160H95.8z M121.8,160l38.2-38.2v6.7L128.5,160H121.8z M147.4,160l12.6-12.6v6.7l-5.9,5.9L147.4,160L147.4,160z"></path></svg>
+        </div>
+        <img class="u-image u-image-default u-image-1" src="images/e1ef9e25ac7e5a6fa52c3c0d867419a138736163d49c5c0c504ffaefe61d95607a1f95a292ccdb47d7914a8423217e56a13e67ad8576d87af880bb_1280.jpg" alt="" data-image-width="1280" data-image-height="959">
+        <div class="u-align-center u-container-style u-group u-shape-rectangle u-white u-group-1">
+          <div class="u-container-layout u-valign-middle u-container-layout-1">
+            <h1 class="u-text u-text-black u-text-1">ADDING A HOUSE TO THE DATABASE</h1>
+          </div>
+        </div>
+        <div class="u-shape u-shape-svg u-text-grey-90 u-shape-2">
+          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-807b"></use></svg>
+          <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-807b"><path d="M114.3,152.3l38-38C144.4,130.9,130.9,144.4,114.3,152.3z M117.1,9.1l-108,108c0.8,1.6,1.7,3.2,2.7,4.8l110-110
+	C120.3,10.9,118.7,10,117.1,9.1z M97.5,2L2,97.5c0.4,2,1,4,1.5,5.9l99.9-99.9C101.5,2.9,99.5,2.4,97.5,2z M80,160c2,0,4-0.1,5.9-0.2
+	l73.9-73.9c0.1-2,0.2-3.9,0.2-5.9c0-0.6,0-1.2,0-1.9L78.1,160C78.8,160,79.4,160,80,160z M34.9,146.1c1.5,1,3,2,4.6,2.9L149,39.5
+	c-0.9-1.6-1.9-3.1-2.9-4.6L34.9,146.1z M132.7,19.8L19.8,132.7c1.2,1.3,2.3,2.6,3.6,3.9L136.6,23.4C135.3,22.2,134,21,132.7,19.8z
+	 M59.6,157.4l97.8-97.8c-0.5-1.9-1.1-3.8-1.7-5.7L53.9,155.6C55.8,156.3,57.7,156.9,59.6,157.4z M7.6,45.9L45.9,7.6
+	C29.1,15.5,15.5,29.1,7.6,45.9z M80,0c-2.6,0-5.1,0.1-7.6,0.4l-72,72C0.1,74.9,0,77.4,0,80c0,0.1,0,0.2,0,0.2L80.2,0
+	C80.2,0,80.1,0,80,0z"></path></svg>
+        </div>
+        <div class="u-form u-white u-form-1">
+          <form action="https://forms.nicepagesrv.com/Form/Process" class="u-clearfix u-form-spacing-15 u-form-vertical u-inner-form" style="padding: 15px;" source="email" name="form">
+            <div class="u-form-group u-form-name u-label-top u-form-group-1">
+              <label for="name-6797" class="u-label">HOUSE ID</label>
+              <input type="text" placeholder="enter house_id" id="name-6797" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle" required="">
+            </div>
+            <div class="u-form-group u-form-phone u-label-top u-form-group-2">
+              <label for="email-6797" class="u-label">PRIMARY PHONE NUMBER</label>
+              <input type="tel" placeholder="enter phone number" id="email-6797" name="email" class="u-border-1 u-border-grey-30 u-input u-input-rectangle" required="required">
+            </div>
+            <div class="u-form-group u-form-message u-label-top u-form-group-3">
+              <label for="message-6797" class="u-label">ADDRESS</label>
+              <textarea placeholder="Address" rows="4" cols="50" id="message-6797" name="message" class="u-border-1 u-border-grey-30 u-input u-input-rectangle" required=""></textarea>
+            </div>
+            <div class="u-form-group u-form-select u-label-top u-form-group-4">
+              <label for="select-1c32" class="u-label">CANAL NAME</label>
+              <div class="u-form-select-wrapper">
+                <select id="select-1c32" name="Select the respective canal" class="u-border-1 u-border-grey-30 u-input u-input-rectangle">
+                  <option value="c104">c104</option>
+                  <option value="c105">c105</option>
+                  <option value="c110">c110</option>
+                  <option value="c111">c111</option>
+                  <option value="c120">c120</option>
+                  <option value="c121">c121</option>
+                </select>
+                <svg class="u-caret u-caret-svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" style="fill:currentColor;" xml:space="preserve"><polygon class="st0" points="8,12 2,4 14,4 "></polygon></svg>
+              </div>
+            </div>
+            <div class="u-form-group u-form-select u-label-top u-form-group-5">
+              <label for="select-b759" class="u-label">SOURCE ID</label>
+              <div class="u-form-select-wrapper">
+                <select id="select-b759" name="select source id" class="u-border-1 u-border-grey-30 u-input u-input-rectangle">
+                  <option value="s1010">s1010</option>
+                  <option value="s1013">s1013</option>
+                  <option value="s1014">s1014</option>
+                  <option value="s1016">s1016</option>
+                </select>
+                <svg class="u-caret u-caret-svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" style="fill:currentColor;" xml:space="preserve"><polygon class="st0" points="8,12 2,4 14,4 "></polygon></svg>
+              </div>
+            </div>
+            <div class="u-align-left u-form-group u-form-submit u-label-top u-form-group-6">
+              <a href="#" class="u-btn u-btn-submit u-button-style">Submit</a>
+              <input type="submit" value="submit" class="u-form-control-hidden">
+            </div>
+            <div class="u-form-send-message u-form-send-success">Thank you! Your message has been sent.</div>
+            <div class="u-form-send-error u-form-send-message">Unable to send your message. Please fix errors then try again.</div>
+            <input type="hidden" value="" name="recaptchaResponse">
+            <input type="hidden" name="formServices" value="d568939bf274436eab60a7597d46000b">
+          </form>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-clearfix u-footer u-grey-80" id="sec-23e0"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-align-left u-border-1 u-border-white u-expanded-width u-line u-line-horizontal u-opacity u-opacity-30 u-line-1"></div>
+        <div class="u-clearfix u-expanded-width u-gutter-30 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-15 u-size-30-md u-layout-cell-1">
+                <div class="u-container-layout u-valign-top u-container-layout-1"><!--position-->
+                  <div data-position="" class="u-position"><!--block-->
+                    <div class="u-block">
+                      <div class="u-block-container u-clearfix"><!--block_header-->
+                        <h5 class="u-block-header u-text"><!--block_header_content-->Akhil Swarop<!--/block_header_content--></h5><!--/block_header--><!--block_content-->
+                        <div class="u-block-content u-text"><!--block_content_content-->CB.EN.U4CSE21304<!--/block_content_content--></div><!--/block_content-->
+                      </div>
+                    </div><!--/block-->
+                  </div><!--/position-->
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-size-15 u-size-30-md u-layout-cell-2">
+                <div class="u-container-layout u-valign-top u-container-layout-2"><!--position-->
+                  <div data-position="" class="u-position"><!--block-->
+                    <div class="u-block">
+                      <div class="u-block-container u-clearfix"><!--block_header-->
+                        <h5 class="u-block-header u-text"><!--block_header_content-->Dhayanandh N<!--/block_header_content--></h5><!--/block_header--><!--block_content-->
+                        <div class="u-block-content u-text"><!--block_content_content-->CB.EN.U4CSE21310<!--/block_content_content--></div><!--/block_content-->
+                      </div>
+                    </div><!--/block-->
+                  </div><!--/position-->
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-size-15 u-size-30-md u-layout-cell-3">
+                <div class="u-container-layout u-valign-top u-container-layout-3"><!--position-->
+                  <div data-position="" class="u-position"><!--block-->
+                    <div class="u-block">
+                      <div class="u-block-container u-clearfix"><!--block_header-->
+                        <h5 class="u-block-header u-text"><!--block_header_content-->Ganeshkaran<!--/block_header_content--></h5><!--/block_header--><!--block_content-->
+                        <div class="u-block-content u-text"><!--block_content_content-->CB.EN.U4CSE21312<!--/block_content_content--></div><!--/block_content-->
+                      </div>
+                    </div><!--/block-->
+                  </div><!--/position-->
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-15 u-size-30-md u-layout-cell-4">
+                <div class="u-container-layout u-valign-top u-container-layout-4"><!--position-->
+                  <div data-position="" class="u-position"><!--block-->
+                    <div class="u-block">
+                      <div class="u-block-container u-clearfix"><!--block_header-->
+                        <h5 class="u-block-header u-text"><!--block_header_content-->Pranav Sreerag<!--/block_header_content--></h5><!--/block_header--><!--block_content-->
+                        <div class="u-block-content u-text"><!--block_content_content-->CB.EN.U4CSE21342<!--/block_content_content--></div><!--/block_content-->
+                      </div>
+                    </div><!--/block-->
+                  </div><!--/position-->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div></footer>
+
+  
+</body></html>
